1a9a9c3e0f0f7955b9207dc3dc30b073eeeef4e6
Jens Averkamp <j.averkamp@itm-systems.com>
2015-02-28 21:14:16 +0100

    Add nullable test

---
+++ tests/System/SystemTest.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\CustomOptOut\tests\System;
+
+use Piwik\Plugins\CustomOptOut\tests\fixtures\SimpleFixtureTrackFewVisits;
+use Piwik\Tests\Framework\TestCase\SystemTestCase;
+
+/**
+ * @group CustomOptOut
+ * @group NullableTest
+ * @group Plugins
+ */
+class SystemTest extends SystemTestCase
+{
+    /**
+     * @var SimpleFixtureTrackFewVisits
+     */
+    public static $fixture = null; // initialized below class definition
+
+    /**
+     * @dataProvider getApiForTesting
+     */
+    public function testApi($api, $params)
+    {
+        $this->runApiTests($api, $params);
+    }
+
+    public function getApiForTesting()
+    {
+        $api = array(
+            'API.get',
+            'Goals.getItemsSku'
+        );
+
+        $apiToTest   = array();
+        $apiToTest[] = array($api,
+            array(
+                'idSite'     => 1,
+                'date'       => self::$fixture->dateTime,
+                'periods'    => array('day'),
+                'testSuffix' => ''
+            )
+        );
+
+        return $apiToTest;
+    }
+
+    public static function getOutputPrefix()
+    {
+        return '';
+    }
+
+    public static function getPathToTestDirectory()
+    {
+        return dirname(__FILE__);
+    }
+
+}
+
+SystemTest::$fixture = new SimpleFixtureTrackFewVisits();
